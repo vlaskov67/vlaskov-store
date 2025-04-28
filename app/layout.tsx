69cd83bee7c0bf100b080ee1f98af59c27994330
vlaskov67 <fenomenGT@protonmail.com>
2025-04-28 12:30:32 +0200

    Delete app directory

--- app/layout.tsx
+++
@@ -1,9 +0,0 @@
-// app/layout.tsx
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
-}
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
-      <h1>Welcome to Vlaskov Store</h1>
-      <p>This is a starter template. Bot will modify this.</p>
-    </main>
-  );
-}
