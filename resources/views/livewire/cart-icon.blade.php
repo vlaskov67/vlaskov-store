4616b627ef732595e7801cc9157fab1dc5956aa7
vlaskov <vlaskov@mac.local>
2025-05-07 15:57:10 +0200

    Remove incorrect Laravel files

--- resources/views/livewire/cart-icon.blade.php
+++
@@ -1,8 +0,0 @@
-<a href="{{ route('cart.index') }}" class="relative inline-block">
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 text-blue-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2 9m13-9l2 9m-5-9V6a2 2 0 10-4 0v7" />
-    </svg>
-    @if($count > 0)
-        <span class="absolute top-0 right-0 inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-white bg-red-600 rounded-full">{{ $count }}</span>
-    @endif
-</a>
